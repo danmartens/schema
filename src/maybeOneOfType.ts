b6c5234222be29c7194462cda5f352e369281b68
Dan Martens <dan@friendsoftheweb.com>
2021-03-21 13:30:11 -0400

    Set up ESLint

--- src/maybeOneOfType.ts
+++ src/maybeOneOfType.ts
@@ -1,29 +1,25 @@
 import { Some } from "./Some";
 import { None } from "./None";
-import { MaybeFactory, MessageFactory } from "./types";
+import { MaybeFactory } from "./types";
 import { validationError } from "./validationError";
 
 export function maybeOneOfType<A, B>(
-  options: [MaybeFactory<A>, MaybeFactory<B>],
-  message?: MessageFactory
+  options: [MaybeFactory<A>, MaybeFactory<B>]
 ): MaybeFactory<A | B>;
 
 export function maybeOneOfType<A, B, C>(
-  options: [MaybeFactory<A>, MaybeFactory<B>, MaybeFactory<C>],
-  message?: MessageFactory
+  options: [MaybeFactory<A>, MaybeFactory<B>, MaybeFactory<C>]
 ): MaybeFactory<A | B | C>;
 
 export function maybeOneOfType<A, B, C, D>(
-  options: [MaybeFactory<A>, MaybeFactory<B>, MaybeFactory<C>, MaybeFactory<D>],
-  message?: MessageFactory
+  options: [MaybeFactory<A>, MaybeFactory<B>, MaybeFactory<C>, MaybeFactory<D>]
 ): MaybeFactory<A | B | C | D>;
 
 export function maybeOneOfType<A, B, C, D>(
   options:
     | [MaybeFactory<A>, MaybeFactory<B>]
     | [MaybeFactory<A>, MaybeFactory<B>, MaybeFactory<C>]
-    | [MaybeFactory<A>, MaybeFactory<B>, MaybeFactory<C>, MaybeFactory<D>],
-  message: MessageFactory = () => () => undefined
+    | [MaybeFactory<A>, MaybeFactory<B>, MaybeFactory<C>, MaybeFactory<D>]
 ): MaybeFactory<A | B> | MaybeFactory<A | B | C> | MaybeFactory<A | B | C | D> {
   return (value) => {
     for (const option of options) {
