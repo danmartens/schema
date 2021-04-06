349dbe6f57a4ad9cb9612b8fa7bcc10b95c1833c
Dan Martens <dan@friendsoftheweb.com>
2021-04-05 22:07:06 -0400

    Make shapes inexact by default

--- src/__tests__/maybeShape.test.ts
+++ src/__tests__/maybeShape.test.ts
@@ -5,7 +5,7 @@ import { None } from '../None';
 import { Some } from '../Some';
 
 test('maybeShape', () => {
-  const postSchema = maybeShape(
+  const schema = maybeShape(
     {
       title: maybeString,
       publishedAt: maybeCoercibleDate,
@@ -20,9 +20,9 @@ test('maybeShape', () => {
     author: { name: 'Jane Doe' },
   };
 
-  expect(postSchema(valid)).toBeInstanceOf(Some);
+  expect(schema(valid)).toBeInstanceOf(Some);
 
-  expect(postSchema(valid).valueOf()).toEqual({
+  expect(schema(valid).valueOf()).toEqual({
     title: 'Hello, World',
     publishedAt: new Date('2020-01-01T00:00:00Z'),
     author: {
@@ -30,19 +30,65 @@ test('maybeShape', () => {
     },
   });
 
-  expect(postSchema({ title: 'Hello, World' })).toBeInstanceOf(None);
+  expect(schema({ title: 'Hello, World' })).toBeInstanceOf(None);
 
-  expect(postSchema({ ...valid, title: 42 })).toBeInstanceOf(None);
+  expect(schema({ ...valid, title: 42 })).toBeInstanceOf(None);
 
   expect(() => {
-    postSchema({ title: 42 }).orThrow();
+    schema({ title: 42 }).orThrow();
   }).toThrowError('Expected value of "Post.title" to match Some<string>');
 
   expect(() => {
-    postSchema({ ...valid, author: true }).orThrow();
+    schema({ ...valid, author: true }).orThrow();
   }).toThrowError('Expected value of "Post.author" to match Some<Author>');
+});
+
+xtest('nested shape error messages are clear', () => {
+  const schema = maybeShape(
+    {
+      author: maybeShape({ name: maybeString }, { name: 'Author' }),
+    },
+    { name: 'Post' },
+  );
 
   expect(() => {
-    postSchema({ ...valid, author: { name: 42 } }).orThrow();
+    schema({ author: { name: 42 } }).orThrow();
   }).toThrowError('Expected value of "Author.name" to match Some<String>');
+});
+
+test('allows extra properties', () => {
+  const schema = maybeShape(
+    {
+      title: maybeString,
+    },
+    { name: 'Post' },
+  );
+
+  expect(
+    schema({
+      title: 'Hello, World',
+      abstract: 'A worldwide greeting.',
+    }).valueOf(),
+  ).toEqual({
+    title: 'Hello, World',
+    abstract: 'A worldwide greeting.',
+  });
+});
+
+test('discards extra properties if "exact" is set to true', () => {
+  const schema = maybeShape(
+    {
+      title: maybeString,
+    },
+    { name: 'Post', exact: true },
+  );
+
+  expect(
+    schema({
+      title: 'Hello, World!',
+      abstract: 'A worldwide greeting.',
+    }).valueOf(),
+  ).toEqual({
+    title: 'Hello, World!',
+  });
 });
--- src/maybeShape.ts
+++ src/maybeShape.ts
@@ -4,37 +4,47 @@ import { Schema, SchemaType, Maybe } from './types';
 import { validationError } from './validationError';
 import { validatorTypeNames } from './validatorTypeNames';
 
-export interface Options {
-  name?: string;
-}
-
-export const maybeShape = <
+export function maybeShape<
   TSchema extends Schema<{ [key: string]: any }>,
   TValue extends SchemaType<TSchema>
 >(
   schema: TSchema,
-  options?: Options,
-) => {
-  const validator = (value: any): Maybe<TValue> => {
-    if (value == null || typeof value !== 'object') {
-      return new None(() => 'Expected value to match Some<Object>');
-    }
+  options: { name: string; exact: true },
+): (value: unknown) => Maybe<TValue>;
 
-    const entries: [string, Some<unknown>][] = [];
+export function maybeShape<
+  TSchema extends Schema<{ [key: string]: any }>,
+  TValue extends SchemaType<TSchema>
+>(
+  schema: TSchema,
+  options: { name: string; exact?: false },
+): (value: unknown) => Maybe<TValue & { [key: string]: unknown }>;
 
-    for (const key of Object.keys(schema)) {
-      const maybeValue = schema[key](value[key]);
+export function maybeShape<
+  TSchema extends Schema<{ [key: string]: any }>,
+  TValue extends SchemaType<TSchema>
+>(schema: TSchema, options: { name: string; exact?: boolean }) {
+  const validator = (value: unknown) => {
+    if (typeof value === 'object' && value != null) {
+      const entries: [string, unknown][] =
+        options?.exact === true ? [] : Object.entries(value);
 
-      if (maybeValue instanceof None) {
-        return new None(() =>
-          validationError([schema[key]], { object: options?.name, key }),
-        );
+      for (const key of Object.keys(schema)) {
+        const maybeValue = schema[key]((value as Record<string, unknown>)[key]);
+
+        if (maybeValue instanceof None) {
+          return new None(() =>
+            validationError([schema[key]], { object: options?.name, key }),
+          );
+        }
+
+        entries.push([key, maybeValue.valueOf()]);
       }
 
-      entries.push([key, maybeValue.valueOf()]);
+      return new Some(Object.fromEntries(entries) as TValue);
     }
 
-    return new Some(Object.fromEntries(entries) as TValue);
+    return new None(() => 'Expected value to match Some<Object>');
   };
 
   if (options?.name != null) {
@@ -42,4 +52,4 @@ export const maybeShape = <
   }
 
   return validator;
-};
+}
